b74544da510f2eb271226339a82167df8ea12455
Michael Cramer <michael@bigmichi1.de>
2022-05-18 10:26:54 +0200

    Update build-and-upload.sh

--- .github/build-and-upload.sh
+++ .github/build-and-upload.sh
@@ -4,17 +4,17 @@ echo "Cloning graphhopper"
 git clone https://github.com/graphhopper/graphhopper.git
 cd graphhopper
 echo "Downloading Dockerfile and graphhopper.sh"
-curl -L https://raw.githubusercontent.com/IsraelHikingMap/graphhopper-docker-image-push/main/Dockerfile > Dockerfile
-curl -L https://raw.githubusercontent.com/IsraelHikingMap/graphhopper-docker-image-push/main/graphhopper.sh > graphhopper.sh
+curl -L https://raw.githubusercontent.com/BigMichi1/graphhopper-docker-image-push/main/Dockerfile > Dockerfile
+curl -L https://raw.githubusercontent.com/BigMichi1/graphhopper-docker-image-push/main/graphhopper.sh > graphhopper.sh
 chmod +x ./graphhopper.sh
 echo "Building docker image"
-docker build . -t israelhikingmap/graphhopper:latest
+docker build . -t bigmichi1/graphhopper:latest
 docker login --username $DOCKERHUB_USER --password $DOCKERHUB_TOKEN
 echo "Publishing docker image"
-docker push israelhikingmap/graphhopper:latest
+docker push bigmichi1/graphhopper:latest
 
 TAG=`git for-each-ref --sort=committerdate refs/tags | tail -n 1 | cut -d "/" -f3`
-if docker manifest inspect israelhikingmap/graphhopper:$TAG >/dev/null; then 
+if docker manifest inspect bigmichi1/graphhopper:$TAG >/dev/null; then 
     echo "No need to publish existing version: $TAG";
 else
     mv Dockerfile ../Dockerfile
@@ -23,9 +23,9 @@ else
     mv ../Dockerfile Dockerfile
     mv ../graphhopper.sh graphhopper.sh
     echo "Building docker image for tag: $TAG"
-    docker build . -t israelhikingmap/graphhopper:$TAG
+    docker build . -t bigmichi1/graphhopper:$TAG
     echo "Publishing docker image for tag: $TAG"
-    docker push israelhikingmap/graphhopper:$TAG
+    docker push bigmichi1/graphhopper:$TAG
 fi
 
 
